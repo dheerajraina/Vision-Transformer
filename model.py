5d53367b16938c74563f585b0944ff9b0e8da9b8
Dheeraj Raina <dheerajraina55@gmail.com>
2024-05-24 12:57:18 +0530

    Added Vision Transformer class

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
 from .settings import *
 import keras
 from keras import ops
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
 def data_augmentation(X_train):
@@ -78,3 +80,102 @@ class PatchEncoder(keras.layers.Layer):
         config = super().get_config()
         config.update({"num_patches": self.num_patches})
         return config
+
+
+class VisionTransformer(tf.Module):
+    def __init__(self, X_train, y_train, X_test, y_test):
+        super().__init__()
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
+        self.data_augmenter = data_augmentation(X_train=self.X_train)
+
+    def create_model(self):
+        inputs = keras.Input(shape=input_shape)
+        # Augment data.
+        augmented = self.data_augmenter(inputs)
+        # Create patches.
+        patches = Patches(patch_size)(augmented)
+        # Encode patches.
+        encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
+
+        # Create multiple layers of the Transformer block.
+        for _ in range(transformer_layers):
+            # Layer normalization 1.
+            x1 = keras.layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+            # Create a multi-head attention layer.
+            attention_output = keras.layers.MultiHeadAttention(
+                num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+            )(x1, x1)
+            # residual connection 1.
+            x2 = keras.layers.Add()([attention_output, encoded_patches])
+            # Layer normalization 2.
+            x3 = keras.layers.LayerNormalization(epsilon=1e-6)(x2)
+            # MLP.
+            x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
+            # residual connection 2.
+            encoded_patches = keras.layers.Add()([x3, x2])
+
+        # Create a [batch_size, projection_dim] tensor.
+        representation = keras.layers.LayerNormalization(
+            epsilon=1e-6)(encoded_patches)
+        representation = keras.layers.Flatten()(representation)
+        representation = keras.layers.Dropout(0.5)(representation)
+        # Add MLP.
+        features = mlp(representation, hidden_units=mlp_head_units,
+                       dropout_rate=0.5)
+        # Classify outputs.
+        logits = keras.layers.Dense(num_classes)(features)
+        # Create the Keras model.
+        model = keras.Model(inputs=inputs, outputs=logits)
+        return model
+
+    def run_experiment(self, model):
+        optimizer = keras.optimizers.AdamW(
+            learning_rate=learning_rate, weight_decay=weight_decay
+        )
+
+        model.compile(
+            optimizer=optimizer,
+            loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=[
+                keras.metrics.SparseCategoricalAccuracy(name="accuracy"),
+                keras.metrics.SparseTopKCategoricalAccuracy(
+                    5, name="top-5-accuracy"),
+            ],
+        )
+
+        checkpoint_filepath = "/tmp/checkpoint.weights.h5"
+        checkpoint_callback = keras.callbacks.ModelCheckpoint(
+            checkpoint_filepath,
+            monitor="val_accuracy",
+            save_best_only=True,
+            save_weights_only=True,
+        )
+
+        history = model.fit(
+            x=self.X_train,
+            y=self.y_train,
+            batch_size=batch_size,
+            epochs=num_epochs,
+            validation_split=0.1,
+            callbacks=[checkpoint_callback],
+        )
+
+        model.load_weights(checkpoint_filepath)
+        _, accuracy, top_5_accuracy = model.evaluate(self.X_test, self.y_test)
+        print(f"Test accuracy: {round(accuracy * 100, 2)}%")
+        print(f"Test top 5 accuracy: {round(top_5_accuracy * 100, 2)}%")
+
+        return history
+
+    def plot_history(self, history, item):
+        plt.plot(history.history[item], label=item)
+        plt.plot(history.history["val_" + item], label="val_" + item)
+        plt.xlabel("Epochs")
+        plt.ylabel(item)
+        plt.title("Train and Validation {} Over Epochs".format(item), fontsize=14)
+        plt.legend()
+        plt.grid()
+        plt.show()
