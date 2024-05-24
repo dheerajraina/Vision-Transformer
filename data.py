573ff2c0f9915404c554ba9dac4889a3943ab609
Dheeraj Raina <dheerajraina55@gmail.com>
2024-05-24 12:15:05 +0530

    update get_data method

--- data.py
+++ data.py
@@ -1,10 +1,7 @@
 import keras
-from sklearn.model_selection import train_test_split
 
 
 def get_data():
-    (X, y), (X_test, y_test) = keras.datasets.cifar100.load_data()
-    X_train, X_val, y_train, y_val = train_test_split(
-        X, y, test_size=0.1, random_state=134)
+    (X_train, y_train), (X_test, y_test) = keras.datasets.cifar100.load_data()
 
-    return X_train, X_val, X_test, y_train, y_val, y_test
+    return X_train, X_test, y_train, y_test
