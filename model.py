aaeacb340937d60e588dcde6823d29bf681396ed
Dheeraj Raina <dheerajraina55@gmail.com>
2024-05-24 12:28:45 +0530

    added a patch encoding layer to tranform patch into vector with an learnable positional embedding (necessary for sequence models as all data is fed at once)

--- model.py
+++ model.py
@@ -55,3 +55,26 @@ class Patches(keras.layers.Layer):
         config = super().get_config()
         config.update({"patch_size": self.patch_size})
         return config
+
+
+class PatchEncoder(keras.layers.Layer):
+    def __init__(self, num_patches, projection_dim):
+        super().__init__()
+        self.num_patches = num_patches
+        self.projection = keras.layers.Dense(units=projection_dim)
+        self.position_embedding = keras.layers.Embedding(
+            input_dim=num_patches, output_dim=projection_dim
+        )
+
+    def call(self, patch):
+        positions = ops.expand_dims(
+            ops.arange(start=0, stop=self.num_patches, step=1), axis=0
+        )
+        projected_patches = self.projection(patch)
+        encoded = projected_patches + self.position_embedding(positions)
+        return encoded
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"num_patches": self.num_patches})
+        return config
