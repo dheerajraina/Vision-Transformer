9c63a25b5b9e495fc98ff92c8bb0250975223e8d
Dheeraj Raina <dheerajraina55@gmail.com>
2024-05-24 13:03:43 +0530

    code fixe

--- data.py
+++ data.py
@@ -4,4 +4,4 @@ import keras
 def get_data():
     (X_train, y_train), (X_test, y_test) = keras.datasets.cifar100.load_data()
 
-    return X_train, X_test, y_train, y_test
+    return X_train, y_train, X_test, y_test
