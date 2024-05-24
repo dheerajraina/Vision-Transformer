174b7c615c5c42915764f6363ff7268eb08e37d8
Dheeraj Raina <dheerajraina55@gmail.com>
2024-05-24 12:20:13 +0530

    migrated data augmentation method to model.py file

---
+++ model.py
@@ -0,0 +1,19 @@
+from .settings import *
+import keras
+
+
+def data_augmentation(X_train):
+    data_augmentation = keras.Sequential(
+        [
+            keras.layers.Normalization(),
+            keras.layers.Resizing(image_size, image_size),
+            keras.layers.RandomFlip("horizontal"),
+            keras.layers.RandomRotation(factor=0.02),
+            keras.layers.RandomZoom(height_factor=0.2, width_factor=0.2),
+        ],
+        name="data_augmentation",
+    )
+    # Compute the mean and the variance of the training data for normalization.
+    data_augmentation.layers[0].adapt(X_train)
+
+    return data_augmentation
