566ecdc431eda009f5f355988687545c44505991
Dheeraj Raina <dheerajraina55@gmail.com>
2024-05-24 12:25:01 +0530

    added method to create multiple patches within an  image as mentioned in paper

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from .settings import *
 import keras
+from keras import ops
 
 
 def data_augmentation(X_train):
@@ -24,3 +25,33 @@ def mlp(x, hidden_units, dropout_rate):
         x = keras.layers.Dense(units, activation=keras.activations.gelu)(x)
         x = keras.layers.Dropout(dropout_rate)(x)
     return x
+
+
+class Patches(keras.layers.Layer):
+    def __init__(self, patch_size):
+        super().__init__()
+        self.patch_size = patch_size
+
+    def call(self, images):
+        input_shape = ops.shape(images)
+        batch_size = input_shape[0]
+        height = input_shape[1]
+        width = input_shape[2]
+        channels = input_shape[3]
+        num_patches_h = height // self.patch_size
+        num_patches_w = width // self.patch_size
+        patches = keras.ops.image.extract_patches(images, size=self.patch_size)
+        patches = ops.reshape(
+            patches,
+            (
+                batch_size,
+                num_patches_h * num_patches_w,
+                self.patch_size * self.patch_size * channels,
+            ),
+        )
+        return patches
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"patch_size": self.patch_size})
+        return config
